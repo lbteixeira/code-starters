db0d2e40157b86b980d060fdf3623d21095707d0
Lucas Teixeira <lucas@lbteixeira.com>
2021-11-24 22:10:51 +0100

    [cpp-app] included glfw in the main function

--- cpp-app/src/project_name/main.cpp
+++ cpp-app/src/project_name/main.cpp
@@ -3,6 +3,7 @@
 #include "lib1/namespace1/add.hpp"
 #include "lib1/namespace2/subtract.hpp"
 #include "lib2/hello.hpp"
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -13,4 +14,28 @@ int main()
   std::cout << lib1::namespace1::add(i, j) << std::endl;
   std::cout << lib1::namespace2::subtract(i, j) << std::endl;
 
+  GLFWwindow* window;
+
+  if( !glfwInit() )
+  {
+      fprintf( stderr, "Failed to initialize GLFW\n" );
+      exit( EXIT_FAILURE );
+  }
+
+  window = glfwCreateWindow( 300, 300, "LT Code Starters", NULL, NULL );
+
+  if (!window)
+  {
+      fprintf( stderr, "Failed to open GLFW window\n" );
+      glfwTerminate();
+      exit( EXIT_FAILURE );
+  }
+
+  while( !glfwWindowShouldClose(window) )
+  {
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+  }
+
+  glfwTerminate();
 }
